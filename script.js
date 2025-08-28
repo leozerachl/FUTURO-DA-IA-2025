cb0446e7800d1455329507ef15975eab6ba6d165
leozerachl <leonardochiniski228@gmail.com>
2025-08-28 11:51:26 -0300

    djenhhbc

---
+++ script.js
@@ -0,0 +1,25 @@
+const caixaPrincipal = document.querySelector("caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultados");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado:
+        "O que é inteligencia Artificial
+        alternativas:["Simulação da inteligência humana por máquinas","Um tipo de inteligência biológica"],
+    }
+    {
+        enunciado:
+        " Qual destas é uma aplicação comum da IA?
+        anternativas:["Diagnóstico médico automatizado","Pintura a óleo"],
+    },
+    {
+     enunciado:
+     " O que é um chatbot
+     alternativas:["Um sistema que simula conversas com humanos"," Um programa que desenha","Um tipo de vírus de computador"],
+     }
+     {
+    enunciado:Qual empresa criou o ChatGPT
+    alternativas:["OpenAI","Google","Google"],
